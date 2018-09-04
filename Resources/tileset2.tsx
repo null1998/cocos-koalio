b7208810126de389f3ebff05d82d819ffa639226
null1998 <1050510316@qq.com>
2018-09-05 02:03:57 +0800

    新地图新音乐资源

---
+++ Resources/tileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset2" tilewidth="32" tileheight="32" tilecount="225" columns="15">
+ <image source="tileset2.png" width="480" height="480"/>
+</tileset>
